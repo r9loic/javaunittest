bfb2d3859fbbc113aebff1094bc71aa461ccc36a
Loic BEAU <loic.beau.16@eigsi.fr>
2015-12-17 11:17:13 +0100

    base code

---
+++ javaunittest_original/edit_javaunittest_form.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * The edit form class for this question type. Each time a question is created moodle uses the
+ * edit form to collect data from the teacher. With this form the following attributes of a question 
+ * need to be defined: name, question text, geven code and the JUnit test class. Affter editing this 
+ * form a question is created in the database with the form's attributes.
+ *
+ * @package    qtype
+ * @subpackage javaunittest
+ * @author     Gergely Bertalan, bertalangeri@freemail.hu
+ * @reference  sojunit 2008, Süreç Özcan, suerec@darkjade.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined ( 'MOODLE_INTERNAL' ) || die ();
+
+/**
+ * javaunittest question type editing form
+ */
+class qtype_javaunittest_edit_form extends question_edit_form {
+    protected function definition_inner ( $mform ) {
+        global $DB, $question, $PAGE;
+        
+        $loaded_initialy = optional_param ( 'reloaded_initialy', 1, PARAM_INT );
+        
+        $qtype = question_bank::get_qtype ( 'javaunittest' );
+        
+        $definitionoptions = $this->_customdata['definitionoptions'];
+        $attachmentoptions = $this->_customdata['attachmentoptions'];
+        
+		
+        
+        // -------------------------- feedback options
+        $mform->addElement ( 'select', 'feedbacklevel', get_string ( 'feedbacklevel', 'qtype_javaunittest' ), 
+                $qtype->feedback_levels () );
+        $mform->setDefault ( 'feedbacklevel', FEEDBACK_ONLY_TIMES );
+        
+		// -------------------------- feedback options
+        #$mform->addElement ( 'select', 'auditlevel', get_string ( 'auditlevel', 'qtype_javaunittest' ), 
+        #        $qtype->audit_values () );
+        #$mform->setDefault ( 'auditlevel', AUDIT_VALUE_30 );
+        
+        // -------------------------- size of the response field
+        $mform->addElement ( 'select', 'responsefieldlines', get_string ( 'responsefieldlines', 'qtype_javaunittest' ), 
+                $qtype->response_sizes () );
+        $mform->setDefault ( 'responsefieldlines', 15 );
+        
+        // -------------------------- "Given Code" Text-Area
+		
+		$init_page  = '<script language="javascript" type="text/javascript" ';
+		#$init_page .= 'src="http://www.cdolivet.com/editarea/editarea/edit_area/edit_area_full.js"></script> ';
+		$init_page .= 'src=" http://lms-test2.eigsi.fr/inc/edit_area/edit_area_full.js"></script> ';
+		$init_page .= '<script language="javascript" type="text/javascript"> ';
+		$init_page .= 'editAreaLoader.init({id : "' . $attributes['id'] . '",syntax: "java",start_highlight: true,is_editable: ' . 'true' .' }); ';
+		$init_page .= ' </script> ';			
+		
+		#$mform->addElement ( $init_page ) ;
+		
+		#<textarea cols="80" rows="20" name="givencode" id="id_givencode">/** Sum of first integers. */&#010;class SommeN {&#010;    /** Computes the sum of the n first integer numbers.&#010;    * @param n the given number [0 +oo[&#010;    * @return the sum of n first integers&#010;    */&#010;    public static int sommeN (int n) {&#010;        // please put your code here&#010;        int res=0;&#010;        for(int i=1;i&lt;=n;i++) {&#010;            res+=i;&#010;        }&#010;        return res;&#010;    }&#010;}</textarea>
+		
+        #$mform->addElement ( 'script' , 'javascript',
+        #        array (
+		#			'src' => 'http://www.cdolivet.com/editarea/editarea/edit_area/edit_area_full.js',
+        #        ) );
+				
+		#$PAGE->requires->js( "http://lms-test2.eigsi.fr/inc/edit_area/edit_area_full.js", true );
+		 
+		$mform->addElement ( 'textarea', 'givencode', get_string ( 'givencode', 'qtype_javaunittest' ), 
+                array (
+                        'cols' => 80,
+                        'rows' => 20,
+                ) );
+				
+				
+		
+        $mform->setType ( 'givencode', PARAM_RAW );
+        $mform->addHelpButton ( 'givencode', 'givencode', 'qtype_javaunittest' );
+        
+        // -------------------------- "Test class" Text-Area
+        $mform->addElement ( 'textarea', 'testclassname', get_string ( 'testclassname', 'qtype_javaunittest' ), 
+                array (
+                        'cols' => 80,
+                        'rows' => 1 
+                ) );
+        $mform->setType ( 'testclassname', PARAM_ALPHANUMEXT );
+        $mform->addRule ( 'testclassname', null, 'required' );
+        $mform->addHelpButton ( 'testclassname', 'testclassname', 'qtype_javaunittest' );
+        $mform->addElement ( 'textarea', 'junitcode', get_string ( 'uploadtestclass', 'qtype_javaunittest' ), 
+                array (
+                        'cols' => 80,
+                        'rows' => 20 
+                ) );
+        $mform->setType ( 'junitcode', PARAM_RAW );
+        $mform->addRule ( 'junitcode', null, 'required' );
+        $mform->addHelpButton ( 'junitcode', 'uploadtestclass', 'qtype_javaunittest' );
+    }
+    public function qtype () {
+        return 'javaunittest';
+    }
+}
---
+++ questiontype.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * The question type class for this question type.
+ *
+ * @package    qtype
+ * @subpackage javaunittest
+ * @author     Gergely Bertalan, bertalangeri@freemail.hu
+ * @reference  sojunit 2008, Süreç Özcan, suerec@darkjade.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined ( 'MOODLE_INTERNAL' ) || die ();
+
+require_once ($CFG->libdir . '/questionlib.php');
+
+define ( 'FEEDBACK_NOTHING', -1 );
+define ( 'FEEDBACK_ONLY_TIMES', 0 );
+define ( 'FEEDBACK_TIMES_COUNT_OF_TESTS', 1 );
+define ( 'FEEDBACK_ALL_EXCEPT_STACKTRACE', 2 );
+define ( 'FEEDBACK_ALL', 3 );
+
+define ( 'AUDIT_VALUE_10', 10 );
+define ( 'AUDIT_VALUE_20', 20 );
+define ( 'AUDIT_VALUE_30', 30 );
+define ( 'AUDIT_VALUE_40', 40 );
+define ( 'AUDIT_VALUE_50', 50 );
+define ( 'AUDIT_VALUE_60', 60 );
+define ( 'AUDIT_VALUE_70', 70 );
+define ( 'AUDIT_VALUE_80', 80 );
+define ( 'AUDIT_VALUE_90', 90 );
+
+/**
+ * The javaunittest question type.
+ *
+ * @copyright 2005 Mark Nielsen
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_javaunittest extends question_type {
+    /**
+     * Whether this question type can perform a frequency analysis of student responses.
+     * If this method returns true, you must implement the get_possible_responses method, and the question_definition
+     * class must implement the classify_response method.
+     *
+     * @return bool whether this report can analyse all the student responses for things like the quiz statistics
+     *         report.
+     */
+    public function can_analyse_responses() {
+        return false;
+    }
+    
+    /**
+     * If your question type has a table that extends the question table, and you want the base class to automatically
+     * save, backup and restore the extra fields, override this method to return an array wherer the first element is
+     * the table name, and the subsequent entries are the column names (apart from id and questionid).
+     *
+     * @return mixed array as above, or null to tell the base class to do nothing.
+     */
+    public function extra_question_fields() {
+        return array (
+                'qtype_javaunittest_options',
+                'responsefieldlines',
+                'givencode',
+                'testclassname',
+                'junitcode',
+                'feedbacklevel'#,
+				#'auditvalue'
+        );
+    }
+    
+    /**
+     * Whether or not to break down question stats and response analysis, for a question defined by $questiondata.
+     *
+     * @param object $questiondata The full question definition data.
+     * @return bool
+     */
+    public function break_down_stats_and_response_analysis_by_variant($questiondata) {
+        return false;
+    }
+    
+    /**
+     *
+     * @return array the different response formats that the question type supports. internal name => human-readable
+     *         name.
+     */
+    public function response_formats() {
+        return array (
+                'plain' => get_string ( 'formatplain', 'qtype_javaunittest' ) 
+        );
+    }
+    
+    /**
+     *
+     * @return array the choices that should be offered for the input box size.
+     */
+    public function response_sizes() {
+        $choices = array ();
+        for ( $lines = 5; $lines <= 40; $lines += 5 ) {
+            $choices[$lines] = get_string ( 'nlines', 'qtype_javaunittest', $lines );
+        }
+        return $choices;
+    }
+    
+    /**
+     *
+     * @return array of feedback levels for question edit page
+     */
+    public function feedback_levels() {
+        $levels = array (
+                FEEDBACK_NOTHING => get_string ( 'feedback_nothing', 'qtype_javaunittest' ),
+                FEEDBACK_ONLY_TIMES => get_string ( 'feedback_only_times', 'qtype_javaunittest' ),
+                FEEDBACK_TIMES_COUNT_OF_TESTS => get_string ( 'feedback_times_count_of_tests', 'qtype_javaunittest' ),
+                FEEDBACK_ALL_EXCEPT_STACKTRACE => get_string ( 'feedback_all_except_stacktrace', 'qtype_javaunittest' ), 
+                FEEDBACK_ALL => get_string ( 'feedback_all', 'qtype_javaunittest' ) 
+        );
+        
+        return $levels;
+    }
+	/**
+     *
+     * @return array of audio values for question edit page
+     */
+    public function audit_values() {
+        $levels = array (
+                AUDIT_VALUE_10 => get_string ( 'AUDIT_VALUE_10', 'qtype_javaunittest' ),
+				AUDIT_VALUE_20 => get_string ( 'AUDIT_VALUE_20', 'qtype_javaunittest' ),
+				AUDIT_VALUE_30 => get_string ( 'AUDIT_VALUE_30', 'qtype_javaunittest' ),
+				AUDIT_VALUE_40 => get_string ( 'AUDIT_VALUE_40', 'qtype_javaunittest' ),
+				AUDIT_VALUE_50 => get_string ( 'AUDIT_VALUE_50', 'qtype_javaunittest' ),
+				AUDIT_VALUE_60 => get_string ( 'AUDIT_VALUE_60', 'qtype_javaunittest' ),
+				AUDIT_VALUE_70 => get_string ( 'AUDIT_VALUE_70', 'qtype_javaunittest' ),
+				AUDIT_VALUE_80 => get_string ( 'AUDIT_VALUE_80', 'qtype_javaunittest' ),
+				AUDIT_VALUE_90 => get_string ( 'AUDIT_VALUE_90', 'qtype_javaunittest' )
+        );
+        
+        return $levels;
+    }
+    
+    public function move_files($questionid, $oldcontextid, $newcontextid) {
+        parent::move_files ( $questionid, $oldcontextid, $newcontextid );
+        $fs = get_file_storage ();
+        $fs->move_area_files_to_new_context ( $oldcontextid, $newcontextid, 'qtype_javaunittest', 'graderinfo', 
+                $questionid );
+    }
+    protected function delete_files($questionid, $contextid) {
+        parent::delete_files ( $questionid, $contextid );
+        $fs = get_file_storage ();
+        $fs->delete_area_files ( $contextid, 'qtype_javaunittest', 'graderinfo', $questionid );
+    }
+}
